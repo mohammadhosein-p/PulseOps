516c49cdb03ce858f134e18286dcc1bd60e1e7e6
mohammadhosein-p <150898771+mohammadhosein-p@users.noreply.github.com>
2026-09-02 17:06:56 +0430

    feat(worker): added payment worker

---
+++ apps/workers/payment/src/index.ts
@@ -0,0 +1,104 @@
+import { Kafka } from "kafkajs";
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+import pino from "pino";
+
+dotenv.config();
+
+const logger = pino({
+    transport: { target: "pino-pretty" },
+    base: { service: "payment-worker" },
+});
+
+const prisma = new PrismaClient();
+const kafka = new Kafka({
+    clientId: "payment-worker",
+    brokers: [process.env.KAFKA_BROKER || "localhost:9092"],
+});
+
+const consumer = kafka.consumer({ groupId: "payment-service-group" });
+const producer = kafka.producer();
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function run() {
+    await consumer.connect();
+    await producer.connect();
+    logger.info("Payment Worker successfully connected to Kafka");
+
+    await consumer.subscribe({ topic: "order-created", fromBeginning: false });
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            if (!message.value) return;
+
+            const orderData = JSON.parse(message.value.toString());
+            const orderId = orderData.orderId;
+
+            logger.info(
+                { orderId, partition },
+                "Received order for payment processing",
+            );
+
+            try {
+                // payment delay simulation
+                const delay = Number(process.env.PROCESSING_DELAY_MS) || 1500;
+                await sleep(delay);
+
+                // order status update
+                await prisma.order.update({
+                    where: { id: orderId },
+                    data: {
+                        status: "PAYMENT_COMPLETED",
+                        events: {
+                            create: {
+                                status: "PAYMENT_COMPLETED",
+                                message: `Payment of $${orderData.totalAmount} processed successfully.`,
+                            },
+                        },
+                    },
+                });
+
+                // pass to Inventory Worker by calling event
+                await producer.send({
+                    topic: "payment-completed",
+                    messages: [
+                        {
+                            key: orderId,
+                            value: JSON.stringify({
+                                orderId,
+                                items: orderData.items,
+                                timestamp: new Date().toISOString(),
+                            }),
+                        },
+                    ],
+                });
+
+                logger.info(
+                    { orderId },
+                    "Payment processed and published to payment-completed",
+                );
+            } catch (error) {
+                logger.error({ error, orderId }, "Failed to process payment");
+
+                await prisma.order.update({
+                    where: { id: orderId },
+                    data: {
+                        status: "PAYMENT_FAILED",
+                        events: {
+                            create: {
+                                status: "PAYMENT_FAILED",
+                                message: "Payment gateway transaction failed.",
+                            },
+                        },
+                    },
+                });
+            }
+        },
+    });
+}
+
+run().catch((err) => {
+    logger.error({ err }, "Fatal error running Payment Worker");
+    process.exit(1);
+});
